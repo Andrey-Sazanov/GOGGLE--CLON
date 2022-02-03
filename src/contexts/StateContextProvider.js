7eebff49f43f355fbc673d1823f7814e85685975
PrivateChpok <sazanov044@gmail.com>
2022-02-03 22:56:33 +0200

    first

---
+++ src/contexts/StateContextProvider.js
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const StateContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const StateContextProvider = ({ children }) => {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const getResults = async (url) => {
+    setLoading(true);
+
+    const res = await fetch(`${baseUrl}${url}`, {
+      method: 'GET',
+      headers: {
+        'x-user-agent': 'desktop',
+        'x-proxy-location': 'EU',
+       'x-rapidapi-host': 'google-search3.p.rapidapi.com',
+        'x-rapidapi-key': 'a9cf9f2fe8msh3307156b45c2727p1a6ec5jsn92a170a4e963'
+      },
+    });
+
+    const data = await res.json();
+
+    setResults(data);
+    setLoading(false);
+  };
+
+  return (
+    <StateContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
